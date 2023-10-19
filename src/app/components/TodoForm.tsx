dc14eb7a911953fac8c8456ebedd8ba6f22a4148
aimansae <saeedaimann@gmail.com>
2023-10-19 21:09:02 +0200

    Final commit

--- src/app/components/TodoForm.tsx
+++ src/app/components/TodoForm.tsx
@@ -35,34 +35,34 @@ const TodoForm = ({ onSubmit, editingTodo }: Props) => {
         })}
       >
         <Styled.InputContainer>
-          <label className='label'>Task:</label>
+          <label className="label">Task:</label>
           <input
             {...register('title')}
-            name='title'
-            type='text'
-            placeholder='Enter task name...'
+            name="title"
+            type="text"
+            placeholder="Enter task name..."
             defaultValue={editingTodo?.title || ''}
             className={editingTodo ? 'editActive' : 'normal'}
           />
 
-          {errors.title && <p className='errors'>{errors.title.message}</p>}
+          {errors.title && <p className="errors">{errors.title.message}</p>}
 
-          <label className='label'>Description:</label>
+          <label className="label">Description:</label>
           <input
             {...register('description')}
-            name='description'
-            type='text'
-            placeholder='Describe the task...'
+            name="description"
+            type="text"
+            placeholder="Describe the task..."
             defaultValue={editingTodo?.description || ''}
             className={editingTodo ? 'editActive' : 'normal'}
           />
           {errors.description && (
-            <p className='errors'>{errors.description.message}</p>
+            <p className="errors">{errors.description.message}</p>
           )}
 
           <Styled.ButtonDiv>
-            <Styled.Button type='submit'>
-              <AiFillPlusSquare className='plusIcon' />
+            <Styled.Button type="submit">
+              <AiFillPlusSquare className="plusIcon" />
               {editingTodo ? 'Update' : 'Add'}
             </Styled.Button>
           </Styled.ButtonDiv>
